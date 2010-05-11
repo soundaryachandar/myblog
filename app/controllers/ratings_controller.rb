f3c17ee27d6dfd6881d2edc8ad0a83ab8d801bac
soundarya chandar <soundaryachandar@Macintosh-2.local>
2010-05-12 00:25:14 +0530

    errorless but buggy ratings implementation

--- app/controllers/ratings_controller.rb
+++ app/controllers/ratings_controller.rb
@@ -1,8 +1,11 @@
 class RatingsController < ApplicationController
-  def create
+ 
+def create
     @post = Post.find(params[:post_id])
     respond_to do |format|
-      @rating = Rating.new(params[:rating])
+      @rating = Rating.new()
+      @rating.no_of_stars= params[:rating][:no_of_stars]
+      @rating.post_id= params[:rating][:post_id]
       @post.rating = @rating
       if @rating.save
         format.js { render :nothing => false }
